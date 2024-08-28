a6c1f81e64513744cddcd46d01d6fdf19ee22ad7
Russell Waterhouse <Russell.L.Waterhouse@gmail.com>
2024-08-27 21:21:40 -0600

    initial commit

---
+++ home.nix
@@ -0,0 +1,36 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "russ";
+  home.homeDirectory = "/Users/russ";
+
+  home.packages = [
+    pkgs.fastfetch
+    pkgs.htop
+    pkgs.hugo
+    pkgs.jq
+    pkgs.ncdu
+    pkgs.neovim
+    pkgs.git
+    pkgs.git-lfs
+    pkgs.tldr
+    pkgs.tmux
+    pkgs.tree
+    pkgs.zoxide
+  ];
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "24.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
