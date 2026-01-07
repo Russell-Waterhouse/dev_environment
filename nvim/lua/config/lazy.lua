b4958310dcc54b5a5837e7c6edd19f958382b483
Russell-Waterhouse <Russell.L.Waterhouse@gmail.com>
2026-01-07 11:55:20 -0700

    update telescope dependency

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -20,8 +20,12 @@ require("lazy").setup({
 
         -- Telescope: fuzzy finder
         {
-            'nvim-telescope/telescope.nvim', tag = '0.1.5',
-            dependencies = { 'nvim-lua/plenary.nvim' }
+          'nvim-telescope/telescope.nvim', tag = 'v0.2.1',
+          dependencies = {
+            'nvim-lua/plenary.nvim',
+            -- optional but recommended
+            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+          }
         },
 
         -- nvim-surround: surround text with whatever characters you give it.
